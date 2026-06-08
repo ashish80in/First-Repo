8ebcfea82d03d36cce6c8cb92093bc9f58d24c89
ashish80in <ashishsharmaashish80@gmail.com>
2026-06-08 14:40:38 +0530

    Add files via upload

---
+++ assignment 5.py
@@ -0,0 +1,108 @@
+# LIST QUESTATIONS 
+
+# square=[x**2 for x in range(0,11)]
+# print(square)
+
+# =================================================
+
+# even=[x for x in range(1,21) if x%2==0]
+# print(even)
+
+# ==================================================
+ 
+# names = input("Enter names: ").split()
+
+# result = [name.upper() for name in names]
+
+# print(result)
+
+
+# ===================================================
+
+
+# names=input("enter you names").split(",")
+# result=[len(name) for name in names]
+# print (result)
+
+# ==================================================== 
+
+
+# n=um = int(input("Enter number: "))
+
+# table = [num * i for i in range(1, 11)]
+# print(table)
+
+# set questations===============================>
+
+# numbers={1,2,3,4,5,6,7,8,9,10,11}
+# print(numbers)
+
+# =====================================================>
+# numbers={1,2,3,4,5,6,7,8,9,10,11}
+# numbers.update(['11','12','13'])
+# print(numbers)
+
+# ======================================================>
+# set={"apple","mango","banana"}
+# set.remove("mango")
+# print(set)
+# set.discard("banana")
+# print(set)
+
+#========================================================>
+# first={"ashish","manjot","sandhu saab of cu ","manju don"}
+# second={"saab manjot","manju don","ambala king manjot"}
+
+# final=(first|second)
+# print(final)
+
+# final=(first & second )
+# print(final)
+
+
+# final=(first-second)
+# print(final)
+# x=input("enter you element")
+# final=["exist"if x in first else "not exist"]
+# print(final)
+
+# first=list(map(int,input("enter the numbers").split(",")))
+# print(first)
+# second=set(first)
+# print(second)
+
+# TUPLE QUESTATION==================================================>
+# subject=tuple(input("enter your subjects").split(","))
+# print(subject)
+# for i in subject:
+#     print(i)
+# =================================================================>
+
+# element=tuple(map(int,input("enter your element").split(",")))
+# print(element)
+# l=int(input("enter you element "))
+# print(l)
+# print(element.count(l)
+# -====================================================================>
+# element=tuple(map(int,input("enter your element").split(",")))
+# print(element)
+
+# element2=tuple(map(int,input("enter your element").split(",")))
+# print(element2)
+
+# element3=element+element2
+# print(element3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
