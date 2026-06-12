2d8c753fff909ad67b2e9fdea9eb4ec055c86ca3
ashish80in <ashishsharmaashish80@gmail.com>
2026-06-12 10:05:08 +0530

    Add files via upload

---
+++ assignment.7.py
@@ -0,0 +1,38 @@
+# questation 2===================================================>
+# def body(weight,height):
+#     return weight/height**2
+# print(body(120,190/100))
+
+# def book_flight(desitination,class_type="economy"):
+#     return f"confirming you ticket to the{desitination},in{class_type}class."
+# desitination=input("enter you desitination")
+# class_type1=input("enter yor class type")
+# if class_type1=="":
+#     print(book_flight(desitination))
+# else:
+#     print(book_flight(desitination,class_type1))
+
+# def create_profile(age,username,country="unknown",):
+#   return age,username,country
+# print(create_profile(username="efefe",age=77,country="efef"))
+
+# def concatenate_words(*words):
+#     return "-".join (words)
+# print(concatenate_words("afbehjf","ejhfgbegfy","ehfbgyhuegf"))
+
+# def build_conf(**seting):
+#     for i,j in seting.items():
+#       print(i,j)
+      
+# build_conf(theme="dark",debug="true",max_user=100)
+
+
+
+
+
+
+
+
+
+
+
---
+++ lecture.7.py
@@ -0,0 +1,43 @@
+#==========================================================functions
+# def add():
+#     a=4
+#     b=4
+#     return a+b
+# print(add())
+
+# def multi(a,b=2):
+#     return a*b
+# print(multi(5,10))
+# ==keyword arguments========================
+# def sub(a=2,b=5,c=9):
+#     d=a+b-c
+#     return(d)
+# print(sub())
+# print(sub(a=10,b=7,c=20))
+# print(sub(1,3,5,8,4,7,5,5))
+
+# def square(num):
+#     return(num*num)
+# print(square(8))
+
+# def greet(name="ashish"):
+#     return "hi"+ name
+# print(greet())
+
+def welcome():
+    return "welcome to python"
+print(welcome())
+
+def double(n):
+    return n*2
+print(double(6))
+
+def area(length,width):
+    return length*width
+print(area(5,3))
+
+def greet(name="ashish"):
+    return "heloo"+name
+print(greet())
+
+
